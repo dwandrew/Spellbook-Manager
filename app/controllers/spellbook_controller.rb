8ec011b4c96083d0b10c8398fd9d9d702bfb5995
dwandrew <danwandrew@gmail.com>
2020-07-08 16:05:36 +0100

    added delete and edit for spellbooks

--- app/controllers/spellbook_controller.rb
+++ app/controllers/spellbook_controller.rb
@@ -68,35 +68,39 @@ class SpellbookController < ApplicationController
       end
     end
     
-      # get "/post/:id/edit" do
-      #   if logged_in?
-      #     if Post.find(params[:id])
-      #       @post = Post.find(params[:id])
-      #       user = current_user
-      #         if user.id == @post.user_id
-      #         erb :'edit'
-      #         else 
-      #           flash[:error] = "Sorry you do not have permission to do that"
-      #           redirect to '/posts'
-      #         end
-      #     else 
-      #       no_post
-      #     end
-      #   else redirect to '/login'
-      #   end
-      # end
+      get "/spellbooks/:id/edit" do
+        if logged_in?
+          @book = Spellbook.find_by_id(params[:id])
+          user = current_user
+          if user.id == @book.user_id
+            type = @book.book_class
+            @spell_list = Spell.all.select{|spell| spell if spell.classes.include? type}.sort_by{|spell| spell[:level]}
+            erb :'/spellbook/edit'
+          else 
+                flash[:error] = "Sorry you do not have permission to do that"
+                redirect to '/spellbooks'
+          end
+        else redirect to '/spellbooks'
+        end
+      end
     
-      # patch "/post/:id" do
-      #   @post = Post.find(params[:id])
-      #   if !params[:post][:content].empty?
-      #   @post.update(title: params[:post][:title].strip, content: params[:post][:content].strip)
-      #     if params[:post][:player_access] == 'on'
-      #     @post.update(player_access: false)
-      #     end
-      #   redirect to "/post/#{ @post.id }"
-      #   else redirect to "/post/#{@post.id}/edit"
-      #   end
-      # end
+      patch "/spellbooks/:id" do
+        book = Spellbook.find_by_id(params[:id])
+          user = current_user
+          if user.id == book.user_id
+            book.update(book_name: params[:update_book][:book_name])
+            book.spells.delete_all
+            spells_list = []
+            params[:update_book][:spells].values.each do |value|
+              Spell.all.select {|spell| spells_list << spell if spell.id == value.to_i }
+              end
+            book.spells = spells_list
+            book.save
+            redirect to "/spellbooks/#{ book.id }"
+        else 
+            redirect to "/spellbooks/#{book.id}/edit"
+        end
+      end
     
       delete "/spellbooks/delete/:id" do
         if logged_in?
